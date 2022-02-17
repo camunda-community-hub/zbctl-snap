eb436f3e05c99873ae3a52f44831bab7ab0a7bee
Falko Menge <falko.menge@camunda.com>
2022-02-16 20:11:40 -0600

    Add script to update version+checksum

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euxo pipefail
+wget "https://github.com/camunda-cloud/zeebe/releases/download/$1/camunda-cloud-zeebe-$1.tar.gz.sha1sum"
+sed -i "s/^version: '[^']*'/version: '$1'/" snap/snapcraft.yaml
+checksum=$(head -c 40 "camunda-cloud-zeebe-$1.tar.gz.sha1sum")
+sed -i "s#    source-checksum: sha1/.*#    source-checksum: sha1/$checksum#" snap/snapcraft.yaml
+rm "camunda-cloud-zeebe-$1.tar.gz.sha1sum"
+git diff --color-words snap/snapcraft.yaml
+git commit -m "Bump zbctl to $1" snap/snapcraft.yaml
