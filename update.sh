19dbc80dff74948b45f64756fdb506fb7bd8235d
Falko Menge <falko.menge@camunda.com>
2022-09-28 19:25:06 +0200

    Add support for alpha releases

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # usage: ./update.sh 1.3.5
 set -euxo pipefail
-VERSION=$(gh release view --repo camunda/zeebe --json tagName --jq .tagName)
+if [ "$1" = -a ]; then
+  # get latest alpha release
+  VERSION=$(gh release list --repo camunda/zeebe | grep Pre-release | head --lines=1 | awk -F'\t' '{print $3}')
+else
+  # get latest stable release
+  VERSION=$(gh release view --repo camunda/zeebe --json tagName --jq .tagName)
+fi
 wget "https://github.com/camunda/zeebe/releases/download/$VERSION/camunda-zeebe-$VERSION.tar.gz.sha1sum"
 sed -i "s/^version: '[^']*'/version: '$VERSION'/" snap/snapcraft.yaml
 checksum=$(head -c 40 "camunda-zeebe-$VERSION.tar.gz.sha1sum")
